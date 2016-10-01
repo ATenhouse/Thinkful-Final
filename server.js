e94904521da96c13f1eefe405ca1d3bf608d23c8
chistery <Adam Tenhouse, Esquire>
2016-10-01 10:48:20 -0500

    Added very simple logging statement.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 app.use(express.static('public'));
 
 var def_port = process.env.PORT;
+
+console.log("Environmental port is " + def_port);
+
 app.listen(def_port);
 
 module.exports = {
