c0efb4107e9b8c9c632a67d79a92a8b25c28bcee
chistery <Adam Tenhouse, Esquire>
2016-10-01 00:04:26 -0500

    Changed default port; tracking down cause of Heroku error.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
 
 module.exports = {
 	app: app
