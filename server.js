0dbb471e09fe01beee177c3fb1534b2dd80d1666
chistery <Adam Tenhouse, Esquire>
2016-10-04 23:24:03 -0500

    added dummy views for important pages ...

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ var def_port = process.env.PORT || 3000;
 
 console.log("Environmental port is " + def_port);
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+app.get('/detail', function(req, res){
+  res.sendFile(__dirname + '/public/detail.html');
+});
+app.get('/login', function(req, res){
+  res.sendFile(__dirname + '/public/login.html');
+});
+
 app.listen(def_port);
 
 module.exports = {
