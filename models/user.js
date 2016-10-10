d38f62bc88000fbc8b7bf766e94f24a21243b47d
Adam Tenhouse <merthedil@gmail.com>
2016-10-10 22:18:46 +0000

    Ch-chchanges

---
+++ models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    password: {type: String, required: true}
+});
+
+var User = mongoose.model('Item', userSchema);
+
+module.exports = User;
